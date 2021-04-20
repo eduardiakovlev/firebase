bbe5707d764596e52157d4685da62583d78798cd
eduardiakovlev <eiakovle@bhcc.edu>
2021-04-20 11:20:25 -0400

    Updates

---
+++ precache-manifest.02969871e3175dcf88b41a9bfdace1a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ce29bbafc91eb560046f3cfa4dc4ee",
+    "url": "/firebase/index.html"
+  },
+  {
+    "revision": "c69d3135dd17959fdc2f",
+    "url": "/firebase/static/css/main.8c54925e.chunk.css"
+  },
+  {
+    "revision": "c3a8da8eea4df0c30c4a",
+    "url": "/firebase/static/js/2.ea1be9d7.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/firebase/static/js/2.ea1be9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c69d3135dd17959fdc2f",
+    "url": "/firebase/static/js/main.e7ea111d.chunk.js"
+  },
+  {
+    "revision": "d1cb4c807a92f9f46df8",
+    "url": "/firebase/static/js/runtime-main.587857ca.js"
+  },
+  {
+    "revision": "4f646371b2a2501c7acd9110625993ca",
+    "url": "/firebase/static/media/background.4f646371.jpg"
+  }
+]);
